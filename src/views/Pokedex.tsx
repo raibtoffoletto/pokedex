609de2b3c37578eaa6bf7f9ce88862ca9b09ea19
Raí B. Toffoletto <rai@toffoletto.me>
2023-02-05 18:39:36 +0000

    fix: import references

--- src/views/Pokedex.tsx
+++ src/views/Pokedex.tsx
@@ -1,8 +1,8 @@
 import Fallback from '@components/Fallback';
 import PageTitle from '@components/PageTitle';
-import Card from '@components/Pokemon/Card';
-import Grid from '@components/Pokemon/Grid';
-import Loader from '@components/Pokemon/Loader';
+import Card from '@components/Pokedex/Card';
+import Grid from '@components/Pokedex/Grid';
+import Loader from '@components/Pokedex/Loader';
 import { usePokedex } from '@hooks/usePokedex';
 
 export default function Pokedex() {
